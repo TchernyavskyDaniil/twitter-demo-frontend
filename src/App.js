75e6f27e2b102fb39721a67645c38b41227569e7
Tchernyavsky <tchernyavsky.daniil@yandex.ru>
2018-06-30 17:08:43 +0300

    fixed naming

--- src/App.js
+++ src/App.js
@@ -2,27 +2,27 @@ import React from "react";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import Profile from "./Profile";
 import Nav from "./Header";
-import OtherUrl from "./Stub";
+import TemplatePage from "./TemplatePage";
 
 export default () => (
   <BrowserRouter>
     <React.Fragment>
       <Nav />
       <Switch>
-        <Route exact path="/moments" component={OtherUrl} />
-        <Route exact path="/messages" component={OtherUrl} />
-        <Route exact path="/notifications" component={OtherUrl} />
-        <Route exact path="/view-all" component={OtherUrl} />
-        <Route exact path="/change" component={OtherUrl} />
-        <Route exact path="/all-people" component={OtherUrl} />
-        <Route exact path="/about" component={OtherUrl} />
-        <Route exact path="/support" component={OtherUrl} />
-        <Route exact path="/terms" component={OtherUrl} />
-        <Route exact path="/privacy-policy" component={OtherUrl} />
-        <Route exact path="/cookies" component={OtherUrl} />
-        <Route exact path="/ads" component={OtherUrl} />
-        <Route exact path="/search" component={OtherUrl} />
-        <Route exact path="/error" component={OtherUrl} />
+        <Route exact path="/moments" component={TemplatePage} />
+        <Route exact path="/messages" component={TemplatePage} />
+        <Route exact path="/notifications" component={TemplatePage} />
+        <Route exact path="/view-all" component={TemplatePage} />
+        <Route exact path="/change" component={TemplatePage} />
+        <Route exact path="/all-people" component={TemplatePage} />
+        <Route exact path="/about" component={TemplatePage} />
+        <Route exact path="/support" component={TemplatePage} />
+        <Route exact path="/terms" component={TemplatePage} />
+        <Route exact path="/privacy-policy" component={TemplatePage} />
+        <Route exact path="/cookies" component={TemplatePage} />
+        <Route exact path="/ads" component={TemplatePage} />
+        <Route exact path="/search" component={TemplatePage} />
+        <Route exact path="/error" component={TemplatePage} />
         <Route path="/:user" component={Profile} />
         <Redirect exact from="/" to="/EveryInteract" />
       </Switch>
